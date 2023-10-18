12f6bde30db7c71138418599ee6d677c47df6dea
unknown <andres17vh@gmail.com>
2023-10-18 09:56:30 -0500

    adding styles and modify footer section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,35 +1,44 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import styles from './footer.module.css'
 
 const Footer = () => {
     return (
-        <section>
-            <div className={styles.footer}>
-                <div className={styles.footerbody}>
-                    <div className={styles.linksections}>
-                        <h5>sections</h5>
-                        <Link href="/about" >About</Link>
-                        <Link href="/portfolio" className='navLink'>Projects</Link>
-                        <Link href="/skills" className='navLink'>Skills</Link>
-                        <Link href="/experience" className='navLink'>Experience</Link>
-                        <Link href="/contact" className='navLink'>Contact</Link>
-
-                    </div >
-                    <div className={styles.linksections}>
-                        <h5> networks</h5>
-                        <ul>
-                            <li><a href="#">lorem</a></li>
-                            <li><a href="#">lorem</a></li>
-                            <li><a href="#">lorem</a></li>
-                        </ul>
-
+        <div className={styles.container}>
+            <div className={styles.footerbody}>
+                <div className={styles.linksections}>
+                    <div className={styles.title}>
+                        <span>Navigation</span>
                     </div>
-                   
+                    <div className={styles.contents}>
+                        <Link href="about" >About</Link>
+                        <Link href="portfolio" className='navLink'>Projects</Link>
+                        <Link href="experience" className='navLink'>Experience</Link>
+                        <Link href="contact" className='navLink'>Contact</Link>
+                    </div>
+
                 </div >
-                <span>Design and built by Andres Cordoba</span>
+                <div className={styles.linksections}>
+                    <div className={styles.title}>
+                        <span>Networks</span>
+                    </div>
+                    <div className={styles.contents}>
+                        <Link href="https://www.linkedin.com/in/andres-cordoba-8714b5238/">Linkedin</Link>
+                        <Link href="mailto:andres17vh@gmail.com" target="_blank">Mail</Link>
+                        <Link href="https://github.com/Andres21y">github</Link>
+                    </div>
+                </div>
+                <div className={styles.linksections}>
+                    <div className={styles.title}>
+                        <span>Grettings</span>
+                    </div>
+                    <a href="https://storyset.com/people" target='_blank'>People illustrations by Storyset</a>
+                </div>
+
             </div >
-        </section>
+            <span className={styles.copyright} >Design and built by Andres Cordoba</span>
+        </div>
     )
 }
 
