ae4a58083de2471b3efef3fc798a10e674e5904b
unknown <andres17vh@gmail.com>
2023-10-20 14:48:02 -0500

    styling category cards

--- src/views/portfolio/page.tsx
+++ src/views/portfolio/page.tsx
@@ -4,74 +4,80 @@ import React from 'react'
 import styles from './portfolio.module.css'
 import { useAppContext } from '@/utils/context';
 import Link from 'next/link';
-import { BiSolidDevices, BiSolidRightArrowAlt } from 'react-icons/bi';
 import { MdArrowRightAlt, MdOutlineWeb } from 'react-icons/md';
-import { BsIntersect } from 'react-icons/bs';
 import Image from 'next/image';
-import { AiOutlineCaretRight } from 'react-icons/ai';
+import { BsPhone } from 'react-icons/bs';
 
-type Props = {
-  svg: any,
+type Card = {
   title: string,
-  summ: string,
-  categorie: string
+  summary: string,
+  category: string,
+  icon: any,
+}
+
+const ProjectCard = ({ title, summary, category, icon }: Card) => {
+  return (
+    <div className={`${styles.card} ${styles[category]}`}>
+      <div className={styles.card_content}>
+
+        {icon}
+        <div className={styles.info}>
+          <span>{title}</span>
+          <span>{summary}</span>
+        </div>
+
+        <Link href="#">
+          <div className={styles.btn_more}>
+            <span>enter</span>
+            <MdArrowRightAlt />
+          </div>
+        </Link>
+
+      </div>
+    </div>
+  )
 }
 
 export default function Portfolio() {
   const { state: { projects } } = useAppContext();
   const latest = projects.slice(0, 2);
+
+
   return (
     <section id="projects" className={styles.gallery} >
       <div className={styles.container}>
 
         <Header name={"gallery"} />
 
+        <span className={styles.title_projects}>
+          Categories
+        </span>
         <div className={styles.projects_container}>
 
-          <div className={`${styles.card} ${styles.card_web}`}>
-            <div className={styles.card_content}>
-              <div className={styles.info}>
-                <MdOutlineWeb />
+          <ProjectCard
+            title="Web"
+            summary="Let's look at projects built with cutting-edge web technologies like React.js, Next.js, TypeScript, Node.js, Express, PostgreSQL, and more. Witness the power of innovation."
+            category="web"
+            icon={<MdOutlineWeb />}
+          />
 
-                <span>Web</span>
-                <span>multi&#45;divice</span>
-              </div>
-              <Link href={"#"} className={styles.btn_more}>
-              <span>Let&#39;s go</span>
-                <MdArrowRightAlt />
-              </Link>
-            </div>
-          </div>
+          <ProjectCard
+            title="Mobile"
+            summary=" React Native Fundamentals
+            - Building Cross-Platform Apps
+            - UI/UX Design for Mobile
+            - Integrating APIs and Services
+            - App Deployment and Distribution"
+            category="mobile"
+            icon={<BsPhone />}
+          />
 
-          <div className={`${styles.card} ${styles.card_mobile}`}>
-            <div className={styles.card_content}>
-              <div className={styles.info}>
-                <MdOutlineWeb />
-
-                <span>Web</span>
-                <span>multi&#45;divice</span>
-              </div>
-              <Link href={"#"} className={styles.btn_more}>
-              <span>Let&#39;s go</span>
-                <MdArrowRightAlt />
-              </Link>
-            </div>
-          </div>
-
-          <div className={`${styles.card} ${styles.card_other}`}>
-            <div className={styles.card_content}>
-              <div className={styles.info}>
-                <MdOutlineWeb />
-
-                <span>Web</span>
-                <span>multi&#45;divice</span>
-              </div>
-              <Link href={"#"} className={styles.btn_more}>
-                <span>Let&#39;s go</span>
-                <MdArrowRightAlt />
-              </Link>
-            </div>
-          </div>
+          <ProjectCard
+            title="UI"
+            summary="Explore web development projects created with HTML, CSS, and JavaScript. Work&#35; showcases the art and science of the web."
+            category="others"
+            icon={<MdOutlineWeb />}
+          />
 
         </div>
         <div className={styles.notables}>
@@ -91,10 +97,29 @@ export default function Portfolio() {
                     className={styles.image}
                   ></Image>
 
-                  <Link href={"#"} className={styles.btn_detail}>
-                    <span>Click me</span>
-                    <AiOutlineCaretRight />
-                  </Link>
+
+                  <div className={styles.btn_container}>
+
+                    <div className={styles.links}>
+                      <Link href={e.code}>
+                        <span>code</span>
+                      </Link>
+                    </div>
+
+                    <div className={styles.links}>
+                      <Link href={e.deploy}>
+                        <span>deploy</span>
+                      </Link>
+                    </div>
+
+                    <Link href={`/categories/${e.id}`}>
+                      <div className={styles.links}>
+                        <span>detail</span>
+                      </div>
+                    </Link>
+
+                  </div>
+
                 </div>
               ))
             }
