ffeb8f875d4c58a902045d6638f717873092be15
unknown <andres17vh@gmail.com>
2023-10-27 08:55:09 -0500

    styling portfolio view

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,10 +3,8 @@ import React from 'react';
 import { Project } from '@/utils/interfaces';
 import Image from 'next/image';
 import styles from './card.module.css'
-import { BsRocketTakeoff } from 'react-icons/bs';
-import { BiCodeAlt } from 'react-icons/bi';
 import Link from 'next/link';
-import { VideoPlayer } from '..';
+
 
 const Card = ({ image, summary, name, deploy, tech, code, category }: Project) => {
 
@@ -33,11 +31,12 @@ const Card = ({ image, summary, name, deploy, tech, code, category }: Project) =
 
                         )
                 } */}
-                <figure>
+                <figure >
                     <Image src={image}
-                        // layout="responsive"
+                        layout="responsive"
                         width={100}
                         height={100}
+                        className={styles.image}
                         alt={name}
                     />
                 </figure>
@@ -56,20 +55,18 @@ const Card = ({ image, summary, name, deploy, tech, code, category }: Project) =
                     }
                 </div>
                 <div className={styles.cardLinks}>
-                    <a href={code}><BiCodeAlt size='21px' color='#ff0000' /><span>code</span></a>
+                    <Link href={code} className={`${styles.link_container}`}>
+                        <div className={`${styles.link_content}`}>
+                            <span>code</span>
+                        </div>
+                    </Link>
 
                     {
                         deploy && (
-                            <Link href={deploy} className={styles.demo}>
-                                <Image
-                                    src={'/svg/rocket.svg'}
-                                    width={40}
-                                    height={40}
-                                    alt='anima'
-                                    priority
-                                    className={styles.demoImg}
-                                />
-                                <span>lunch</span>
+                            <Link href={deploy} className={`${styles.link_container} ${styles.demo}`} >
+                                <div className={`${styles.link_content}`}>
+                                    <span>deploy</span>
+                                </div>
                             </Link>
                         )
                     }
--- src/views/portfolio/page.tsx
+++ src/views/portfolio/page.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 import styles from './portfolio.module.css'
 import { useAppContext } from '@/utils/context';
 import Link from 'next/link';
-import { MdArrowRightAlt, MdOutlineWeb } from 'react-icons/md';
+import { MdArrowRightAlt, MdOpenInNew, MdOutlineWeb } from 'react-icons/md';
 import Image from 'next/image';
-import { BsPhone } from 'react-icons/bs';
+import { BsPhone, BsRocketTakeoff } from 'react-icons/bs';
+import { BiCodeAlt } from 'react-icons/bi';
 
 type Card = {
   title: string,
@@ -16,42 +17,48 @@ type Card = {
 }
 
 const ProjectCard = ({ title, summary, category, icon }: Card) => {
+
   return (
     <div className={`${styles.card} ${styles[category]}`}>
       <div className={styles.card_content}>
 
-        {icon}
+        <span className={styles.icon}>{icon}</span>
+
         <div className={styles.info}>
-          <span>{title}</span>
-          <span>{summary}</span>
+          <h2>{title}</h2>
+          <p>{summary}</p>
         </div>
 
-        <Link href="#">
-          <div className={styles.btn_more}>
+        <div className={styles.btn_more}>
+          <Link
+            href={`/categories/${category}`}
+          >
             <span>enter</span>
-            <MdArrowRightAlt />
-          </div>
-        </Link>
+          </Link>
+
+          <MdArrowRightAlt />
+        </div>
 
       </div>
+
     </div>
   )
 }
 
 export default function Portfolio() {
   const { state: { projects } } = useAppContext();
-  const latest = projects.slice(0, 2);
-
-
+  const notable = projects.slice(0, 2);
   return (
-    <section id="projects" className={styles.gallery} >
+
+    <section id="portfolio" className={styles.gallery} >
       <div className={styles.container}>
 
         <Header name={"gallery"} />
 
-        <span className={styles.title_projects}>
+        <span className='sub_title'>
           Categories
         </span>
+
         <div className={styles.projects_container}>
 
           <ProjectCard
@@ -75,50 +82,59 @@ export default function Portfolio() {
           <ProjectCard
             title="UI"
             summary="Explore web development projects created with HTML, CSS, and JavaScript. Work&#35; showcases the art and science of the web."
-            category="others"
+            category="ui"
             icon={<MdOutlineWeb />}
           />
 
         </div>
+
         <div className={styles.notables}>
-          <span className={styles.title_projects}>
-            Notable projects
-          </span>
+
+          <span className='sub_title'> Notable projects </span>
+
           <div className={styles.notable_projects}>
             {
-              projects.length && latest.map(e => (
+              projects.length && notable.map(e => (
                 <div className={styles.card_notable} key={e.id}>
-                  <Image
-                    layout="responsive"
-                    src={e.image}
-                    height={100}
-                    width={100}
-                    alt={e.name}
-                    className={styles.image}
-                  ></Image>
+                  <figure>
+                    <Image
+                      layout="responsive"
+                      src={e.image}
+                      height={100}
+                      width={100}
+                      alt={e.name}
+                      className={styles.image}
+                    ></Image>
+                    <figcaption>
+
+                      <span className={styles.title}>{e.name}</span>
+                    </figcaption>
+                  </figure>
 
 
                   <div className={styles.btn_container}>
-
-                    <div className={styles.links}>
+                
                       <Link href={e.code}>
-                        <span>code</span>
+                        <div className={`${styles.links} ${styles.web}`} title='code'>
+                          <BiCodeAlt size={25} />
+                        </div>
                       </Link>
-                    </div>
 
-                    <div className={styles.links}>
                       <Link href={e.deploy}>
-                        <span>deploy</span>
+                        <div className={`${styles.links} ${styles.mobile}`} title='deploy'>
+                          <BsRocketTakeoff size={25} />
+                        </div>
                       </Link>
+
+                      <Link href={`/categories/web`}>
+                        <div className={`${styles.links} ${styles.ui}`} title='detail'>
+                          <MdOpenInNew size={25} />
+                        </div>
+                      </Link>
+
+
                     </div>
-
-                    <Link href={`/categories/${e.id}`}>
-                      <div className={styles.links}>
-                        <span>detail</span>
-                      </div>
-                    </Link>
-
-                  </div>
+               
 
                 </div>
               ))
@@ -130,25 +146,3 @@ export default function Portfolio() {
     </section>
   )
 }
-/**
- * 
- *  {
-            projects.length && projects.map(e => (
-              <div className={styles.card} key={e.id}>
-
-                <Card
-                  id={e.id}
-                  name={e.name}
-                  code={e.code}
-                  summary={e.summary}
-                  image={e.image}
-                  deploy={e.deploy}
-                  tech={e.tech}
-                  linkedin={e.linkedin}
-                  category={e.category}
-
-                />
-              </div>
-            ))
-          }
- */
